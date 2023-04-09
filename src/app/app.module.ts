75f4b0389038ee76bcb82a46aa3a7d8cee276fbe
Asimozcan <asimaoezcan@gmail.com>
2023-04-09 03:02:02 +0300

    Auth test wit Mockup APİ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+  
+    // Formdan alınan veriler
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+  
+    // API'ye POST isteği yapma
+    this.http.post('http://localhost:3000/users', { email, password }).subscribe(
+      (response) => {
+        console.log('API cevabı:', response);
+        // İşlem başarılı olduğunda yapılacaklar
+      },
+      (error) => {
+        console.error('API hatası:', error);
+        // Hata durumunda yapılacaklar
+      }
+    );
+  }
+  
 }
